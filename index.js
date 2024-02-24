cf645ca78900f2ecc87030b2a8ebf8dfa52ebd3c
atul3007 <atuldwivedi859@gmail.com>
2024-02-24 11:06:14 +0530

    done

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const translate = require('translate-google');
+require('dotenv').config();
+const app = express();
+const PORT = process.env.PORT ;
+
+app.use(bodyParser.json());
+
+app.post('/translate', (req, res) => {
+  try {
+    // Check if request body is in JSON format
+    const { text } = req.body;
+    if (!text) {
+      return res.status(400).json({ error: "Invalid request body. Missing 'text' key." });
+    }
+
+    // Translate the English text to French
+    translate(text, { to: 'fr' })
+      .then((frenchText) => {
+        // Respond with the translated text in French
+        const response = { translation: frenchText };
+        res.json(response);
+      })
+      .catch((error) => {
+        res.status(500).json({ error: error.message || 'Translation error' });
+      });
+  } catch (error) {
+    res.status(500).json({ error: error.message || 'Internal server error' });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
